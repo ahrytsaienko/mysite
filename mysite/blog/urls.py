0dc24eaee75c50cbf9dcb097e897c638525155fc
temsan89 <temsan89@gmail.com>
2017-12-03 15:15:01 +0200

    post_detail

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # post view
     path(r'', views.post_list, name='post_list'),
     # url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\r'(?P<post>[-\w]+)/$',
-    path(r'<int:year>/<int:month>/<int:day>/<post>[-\w]+/', views.post_detail, name='post_detail'),
+    path(r'<int:year>/<int:month>/<int:day>/<post>/', views.post_detail, name='post_detail'),
 ]
