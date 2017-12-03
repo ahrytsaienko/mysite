f5c4c9dd15d0c66a5b6af08c5152580148f6219e
temsan89 <temsan89@gmail.com>
2017-12-03 18:28:43 +0200

    post list class based view

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # post view
-    path(r'', views.post_list, name='post_list'),
+    path(r'', views.PostListView.as_view(), name='post_list'),
     # url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\r'(?P<post>[-\w]+)/$',
     path(r'<int:year>/<int:month>/<int:day>/<post>/', views.post_detail, name='post_detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,22 +1,14 @@
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 2)
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-
-    return render(request, 'blog/post/list.html', {'page': page,
-                                                   'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
